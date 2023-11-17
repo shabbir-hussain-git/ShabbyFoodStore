f6e341197476c2f6167b2c93197656c12700f887
Shabbir Hussain <pshabbir36@gmail.com>
2023-11-17 15:40:45 +0530

    Code corrected

--- FoodZee/components/Home/FoodItem/FoodItemActivity.js
+++ FoodZee/components/Home/FoodItem/FoodItemActivity.js
@@ -11,24 +11,20 @@ import Loader from '../../CommonUI/Loader';
 
 const FoodItemActivity = () => {
   // uncomment for heavy load
-  let heavyView = [];
-  for (let index = 0; index < 500; index++) {
-    heavyView.push(<HeavyFoodList key={index} />);
-  }
+  // let heavyView = [];
+  // for (let index = 0; index < 500; index++) {
+  //   heavyView.push(<HeavyFoodList key={index} />);
+  // }
 
   // uncomment for fast load
-  // const [heavyView, heavyViewHandler] = useState([]);
-
-  // const [showLoader, showLoaderHandler] = useState(true);
-
-  // useEffect(() => {
-  //   let temp = [];
-  //   for (let index = 0; index < 500; index++) {
-  //     temp.push(<HeavyFoodList key={index} />);
-  //   }
-  //   showLoaderHandler(false);
-  //   heavyViewHandler(temp);
-  // }, []);
+  const [heavyView, heavyViewHandler] = useState([]);
+  useEffect(() => {
+    let temp = [];
+    for (let index = 0; index < 500; index++) {
+      temp.push(<HeavyFoodList key={index} />);
+    }
+    heavyViewHandler(temp);
+  }, []);
   return (
     <>
       {/* {showLoader && <Loader />} */}
